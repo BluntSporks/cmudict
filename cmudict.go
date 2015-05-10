5eb20af32d302e516e0044f30738b5716537a97d
Douglas Green <bluntsporks+git@gmail.com>
2015-05-10 10:05:41 -0400

    Moving function here

--- cmudict.go
+++ cmudict.go
@@ -7,6 +7,8 @@ import (
 	"os"
 	"path"
 	"strings"
+
+	"github.com/BluntSporks/cmudict"
 )
 
 // DefaultDictPath gets the CMU dictionary file location from the CMUDICT_DATA environment variable.
@@ -25,6 +27,49 @@ func DefaultSymbolPath() string {
 		log.Fatal("Set CMUDICT_DATA variable to directory of symbols file")
 	}
 	return path.Join(dir, "cmudict.0.7a.symbols")
+}
+
+// FixPhonemes fixes a pronunciation string with custom fixes designed for better phonetic spelling.
+func FixPhonemes(phonemes []string) []string {
+	n := len(phonemes)
+	newPhonemes := make([]string, 0, 2*n)
+	for i := 0; i < n; i++ {
+		phoneme := phonemes[i]
+		if len(phoneme) > 2 && phoneme[:2] == "ER" {
+			// Use ahx r instead of erx.
+			newPhonemes = append(newPhonemes, "AH"+string(phoneme[2]))
+			phoneme = "R"
+		} else if i < n-1 {
+			if phoneme == "HH" && phonemes[i+1] == "W" {
+				// Use wh instead of hw.
+				phoneme = "WH"
+				i++
+			}
+		}
+		newPhonemes = append(newPhonemes, phoneme)
+	}
+	n = len(newPhonemes)
+	out := make([]string, 0, 2*n)
+	for i, ph := range newPhonemes {
+		out = append(out, ph)
+		if i < n-1 {
+			// Use an apostrophe to split up ambiguous combinations of sounds.
+			split := false
+			if newPhonemes[i+1] == "HH" {
+				if ph == "D" || ph == "S" || ph == "T" || ph == "W" || ph == "Z" {
+					split = true
+				}
+			} else if ph == "N" && newPhonemes[i+1] == "G" {
+				split = true
+			} else if cmudict.IsVowel(ph) && cmudict.IsVowel(newPhonemes[i+1]) {
+				split = true
+			}
+			if split {
+				out = append(out, "'")
+			}
+		}
+	}
+	return out
 }
 
 // GetPhonemes returns the list of phonemes from a string, with or without accents.
